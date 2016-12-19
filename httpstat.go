b7e0b93ed5355eb108889ecac5f9558571592f75
tcnksm <nsd22843@gmail.com>
2016-12-19 17:49:36 +0900

    Use go1.8 TLS hooks

--- httpstat.go
+++ httpstat.go
@@ -7,8 +7,6 @@ import (
 	"context"
 	"fmt"
 	"io"
-	"net"
-	"net/http/httptrace"
 	"strings"
 	"time"
 )
@@ -34,8 +32,18 @@ type Result struct {
 	t2 time.Time
 	t3 time.Time
 	t4 time.Time
-
 	t5 time.Time // need to be provided from outside
+
+	dnsStart      time.Time
+	dnsDone       time.Time
+	tcpStart      time.Time
+	tcpDone       time.Time
+	tlsStart      time.Time
+	tlsDone       time.Time
+	serverStart   time.Time
+	serverDone    time.Time
+	transferStart time.Time
+	trasferDone   time.Time // need to be provided from outside
 
 	// isTLS is true when connection seems to use TLS
 	isTLS bool
@@ -74,22 +82,7 @@ func (r *Result) Total(t time.Time) time.Duration {
 	return t.Sub(r.t0)
 }
 
-// End sets the time when reading response is done.
-// This must be called after reading response body.
-func (r *Result) End(t time.Time) {
-	r.t5 = t
-
-	// This means result is empty (it does nothing).
-	// Skip setting value(contentTransfer and total will be zero).
-	if r.t0.IsZero() {
-		return
-	}
-
-	r.contentTransfer = r.t5.Sub(r.t4)
-	r.total = r.t5.Sub(r.t0)
-}
-
-// Format implements fmt.Formatter interface.
+// Format formats stats result.
 func (r Result) Format(s fmt.State, verb rune) {
 	switch verb {
 	case 'v':
@@ -150,90 +143,5 @@ func (r Result) Format(s fmt.State, verb rune) {
 // WithHTTPStat is a wrapper of httptrace.WithClientTrace. It records the
 // time of each httptrace hooks.
 func WithHTTPStat(ctx context.Context, r *Result) context.Context {
-	return httptrace.WithClientTrace(ctx, &httptrace.ClientTrace{
-		GetConn: func(hostPort string) {
-			_, port, err := net.SplitHostPort(hostPort)
-			if err != nil {
-				return
-			}
-
-			// Heuristic way to detect
-			if port == "443" {
-				r.isTLS = true
-			}
-		},
-
-		DNSStart: func(i httptrace.DNSStartInfo) {
-			r.t0 = time.Now()
-		},
-		DNSDone: func(i httptrace.DNSDoneInfo) {
-			r.t1 = time.Now()
-			r.DNSLookup = r.t1.Sub(r.t0)
-			r.NameLookup = r.t1.Sub(r.t0)
-		},
-
-		ConnectStart: func(_, _ string) {
-			// When connecting to IP
-			if r.t0.IsZero() {
-				r.t0 = time.Now()
-				r.t1 = r.t0
-			}
-		},
-
-		ConnectDone: func(network, addr string, err error) {
-			r.t2 = time.Now()
-			if r.isTLS {
-				r.TCPConnection = r.t2.Sub(r.t1)
-				r.Connect = r.t2.Sub(r.t0)
-			}
-		},
-
-		GotConn: func(i httptrace.GotConnInfo) {
-			// Handle when keep alive is enabled and connection is reused.
-			// DNSStart(Done) and ConnectStart(Done) is skipped
-			if i.Reused {
-				r.t0 = time.Now()
-				r.t1 = r.t0
-				r.t2 = r.t0
-
-				r.isReused = true
-			}
-		},
-
-		WroteRequest: func(info httptrace.WroteRequestInfo) {
-			r.t3 = time.Now()
-
-			// This means DNSStart, Done and ConnectStart is not
-			// called. This happens if client doesn't use DialContext
-			// or using net package before go1.7.
-			if r.t0.IsZero() && r.t1.IsZero() && r.t2.IsZero() {
-				r.t0 = time.Now()
-				r.t1 = r.t0
-				r.t2 = r.t0
-				r.t3 = r.t0
-			}
-
-			// When connection is reused, TLS handshake is skipped.
-			if r.isReused {
-				r.t3 = r.t0
-			}
-
-			if r.isTLS {
-				r.TLSHandshake = r.t3.Sub(r.t2)
-				r.Pretransfer = r.t3.Sub(r.t0)
-				return
-			}
-
-			r.TCPConnection = r.t3.Sub(r.t1)
-			r.Connect = r.t3.Sub(r.t0)
-
-			r.TLSHandshake = r.t3.Sub(r.t3)
-			r.Pretransfer = r.Connect
-		},
-		GotFirstResponseByte: func() {
-			r.t4 = time.Now()
-			r.ServerProcessing = r.t4.Sub(r.t3)
-			r.StartTransfer = r.t4.Sub(r.t0)
-		},
-	})
+	return withClientTrace(ctx, r)
 }
